8a89174bd0b2153aa7214e7c3ed428b4025bc5a0
maryna.korovay <maryna.korovay@fadata.eu>
2024-05-24 13:32:01 +0200

    New SESSION  VAR

--- index.php
+++ index.php
@@ -6,12 +6,16 @@
     <body>
         <?php
          session_start();
-         function getWishesForm($errorMessage = '') {
+
+         function cleanDate($data){
+            return htmlspecialchars(stripcslashes(trim($data)));
+         }
+          function getWishesForm($errorMessage = '') {
             echo"<h1> My Wishlist </h1>";
             if ($errorMessage){
                 echo "<p>$errorMessag<p";
             }
-            echo" <form method='post' function= ''>
+            echo" <form method='post' action= ''>
             <label for= wish1> Wish One</label>
             <input type='text' id='wish1' name='wish1'> <br>
             <label for= wish2> Wish Two</label>
@@ -24,27 +28,42 @@
             ";
          }
 
-         function displayWishes($first_wish = '',$second_wish = '',$third_wish = '') {
-            echo" <form method='post' function= ''>
+         function displayWishes($errorMessage= '') {
+            if ($errorMessage){
+                echo "<p>$errorMessag<p";
+            }
+            echo" 
             <h2>Meine Lieferangaben </h2> <br>
-            <p>1.Wish: " . $first_wish . " ihr Wunsch der ersten Seite </p><br>
-            <p>2.Wish: " . $second_wish . " ihr Wunsch der ersten Seite </p><br>
-            <p>3.Wish: " . $third_wish . " ihr Wunsch der ersten Seite </p><br>
+            <p>1.Wish:  ihr Wunsch der ersten Seite </p><br>
+            <p>2.Wish:  ihr Wunsch der ersten Seite </p><br>
+            <p>3.Wish: ihr Wunsch der ersten Seite </p><br>
             <p> </p>
-            <form method='post' function= ''>
+            <form method='post' action= ''>
             <label for= fullName> Vor- und Nachname: </label>
             <input type='text' id='fullName name='fullName'> <br>
             <label for= secondAdressLine> PLZ und Ort: </label>
             <input type='text' id='secondAdressLine name='secondAdressLine'> <br>
             <label for= phoneNumber> Telefon :</label>
-            <input type='text' id='honeNumber' name='honeNumber'> <br>
-            <input type='submit' name='cancelWishes' value='Cancel'>
-            <input type='submit' name='submitWishes' value='Submit'>
+            <input type='text' id='phoneNumber' name='phoneNumber'> <br>
+            <input type='submit' name='cancelAdress' value='Cancel'>
+            <input type='submit' name='submitAdress' value='Submit'>
             </form>
             ";
          }
         //  getWishesForm();
         displayWishes('Gift', 'Cat', 'Picture');
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            if(isset($_POST['submitWishes'])){
+                $wish1 = cleanData($_POST['wish1']);
+                $wish2 = cleanData($_POST['wish2']);
+                $wish3 = cleanData($_POST['wish3']);
+            }  
+            if(preg_match('/[^a-zA-Z0-9 ]/', $wish1) || preg_match('/[^a-zA-Z0-9 ]/', $wish2) ||preg_match('/[^a-zA-Z0-9 ]/', $wish3)) {
+                echo('You have unwanted characters');
+            }else{
+                $_SESSION['wishes'] = [$wish1, $wish2, $wish3]
+            }
+        }
           ?>
     </body>
 </html>
